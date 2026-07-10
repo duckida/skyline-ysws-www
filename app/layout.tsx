b7724750a7306f332b70781672006f9a2ab8d76d
duckida <duckidaweb@gmail.com>
2026-07-10 15:26:43 +0100

    add simple anayltics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { LINE_Seed_JP } from "next/font/google";
 import { Dela_Gothic_One } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Script from 'next/script'
 
 const line = LINE_Seed_JP({ subsets: ['latin'], variable: '--font-sans', weight: ["400"] });
 const dela = Dela_Gothic_One({subsets:['latin'],variable:'--font-sans', weight:["400"]});
@@ -22,7 +23,13 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", "font-sans", line.variable, "font-heading", dela.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script
+            src="https://scripts.simpleanalyticscdn.com/latest.js" // Replace with your analytics URL
+            strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
